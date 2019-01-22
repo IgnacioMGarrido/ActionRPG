29a3a9b43e46d2b6bd68d2e742b501418f353a12
Ignacio Martinez <nama1510@gmail.com>
2019-01-22 01:47:06 +0100

    ARPG02 - Making the Character moving around

--- Actors/Characters/Character.gd
+++ Actors/Characters/Character.gd
@@ -1,10 +1,35 @@
-extends KinematicBody2D
+ extends KinematicBody2D
 
 class_name Character
+
+#For visualization
+signal speed_updated
+signal state_changed
+
 
 var input_direction : Vector2 = Vector2()
 var last_move_direction : Vector2 = Vector2(1,0)
 
+
+const MAX_WALK_SPEED = 400
+const MAX_RUN_SPEED = 700
+
+var speed : float = 0.0
+var max_speed : float = 0.0
+
+var velocity : Vector2 = Vector2()
+
 func _physics_process(delta) -> void:
 	if input_direction:
 		last_move_direction = input_direction
+		if speed != max_speed:
+			speed = max_speed
+	else:
+		speed = 0.0
+	emit_signal('speed_updated', speed)
+	
+	velocity = input_direction.normalized() * speed;
+	move_and_slide(velocity)
+
+#	var motion = input_direction.normalized() * speed * delta
+#	move_and_collide(motion)
--- Actors/Characters/player/Player.gd
+++ Actors/Characters/player/Player.gd
@@ -13,6 +13,8 @@ func _physics_process(delta) -> void:
 	# 8 directions
 	handle_eight_direction_input()
 	
+
+	
 	if input_direction and input_direction != last_move_direction:
 		emit_signal('direction_changed', input_direction)
 
@@ -21,8 +23,11 @@ func handle_four_direction_input() -> void:
 	input_direction.x = float(Input.is_action_pressed('move_right')) - float(Input.is_action_pressed('move_left'))
 	if not input_direction.x:
 		input_direction.y = float(Input.is_action_pressed('move_down')) - float(Input.is_action_pressed('move_up'))
+	max_speed = MAX_RUN_SPEED if Input.is_action_pressed('run') else MAX_WALK_SPEED
 
 
 func handle_eight_direction_input() -> void:
 	input_direction.x = float(Input.is_action_pressed('move_right')) - float(Input.is_action_pressed('move_left'))
 	input_direction.y = float(Input.is_action_pressed('move_down')) - float(Input.is_action_pressed('move_up'))
+	
+	max_speed = MAX_RUN_SPEED if Input.is_action_pressed('run') else MAX_WALK_SPEED
